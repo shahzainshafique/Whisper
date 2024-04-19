429d0092da14fa00253b213295843b8cbaef90c5
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-04-19 20:44:30 +0500

    CSS Fixes

--- client/src/components/people/Bar.tsx
+++ client/src/components/people/Bar.tsx
@@ -9,25 +9,33 @@ type BarProps = {
 
 const Bar: React.FC<BarProps> = ({ name, message, time, notifications }) => {
   return (
-    <div className="py-8 max-w[100%] bg-white space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 border-b-2">
-      <img
-        className="w-12 h-12 rounded-full object-cover"
-        src="/av.jpg"
-        alt="avatar"
-      />
-      <div className="flex flex-row text-center space-y-1 sm:text-left">
-        <div className="space-y-0.5">
-          <p className="text-lg text-black font-bold">{name ?? ""}</p>
-          <p className="text-slate-500 font-medium">{message ?? ""}</p>
+    <div className="w-full flex flex-row justify-between bg-white p-2 border-b-2">
+      <div className="flex flex-row space-x-3 items-center">
+        <img
+          src="/av.jpg"
+          className="h-10 w-10 rounded-full object-cover"
+          alt="av"
+        />
+        <div className="flex flex-col space-y-1">
+          <p className="font-bold">{name ?? name}</p>
+          <p className="text-slate-500 truncate max-w-[14rem]">
+            {message ?? name}
+          </p>
         </div>
-        <div className="flex flex-col space-y-2">
-          <p className="text-slate-500 font-medium">{time ?? ""}</p>
-          {notifications ? (
-            <span className="rounded-full bg-orange-500 text-white text-center h-6 w-6">
+      </div>
+      <div className="flex flex-col justify-between items-center space-y-2">
+        <p className="text-slate-500 font-medium text-sm">{time ?? ""}</p>
+        {notifications ? (
+          <span className="rounded-full bg-orange-500 text-white text-center h-6 w-6 flex items-center justify-center">
+            <span
+              className={`${
+                notifications > 99 ? "text-xs" : "text-sm"
+              } font-semibold`}
+            >
               {notifications}
             </span>
-          ) : null}
-        </div>
+          </span>
+        ) : null}
       </div>
     </div>
   );
--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -5,9 +5,30 @@ import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 const SideBar = () => {
   return (
-    <section className="hidden lg:flex justify-between flex-col gap-3 h-screen w-[6%] bg-[#010019] rounded-r-2xl py-10">
-      <img src="/phoenix.png" alt="logo" className="self-center h-8 " />
-      <div className="flex flex-col gap-[5rem] self-center">
+    <>
+      <section className="hidden lg:flex justify-between flex-col gap-3 h-screen w-[6%] bg-[#010019] rounded-r-2xl py-10">
+        <img src="/phoenix.png" alt="logo" className="self-center h-8 " />
+        <div className="flex flex-col gap-[5rem] self-center">
+          <Link to="/">
+            <FaRegEnvelope color="white" size={20} />
+          </Link>
+          <Link to="/">
+            <IoMdChatbubbles color="white" size={20} />
+          </Link>
+          <Link to="/">
+            <FaRegBell color="white" size={20} />
+          </Link>
+          <Link to="/">
+            <FaRegTrashAlt color="white" size={20} />
+          </Link>
+        </div>
+        <div className="self-center">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </section>
+      <section className="lg:hidden fixed bottom-0 left-0 right-0 bg-[#010019] p-5 flex justify-around items-center border-t border-gray-300">
         <Link to="/">
           <FaRegEnvelope color="white" size={20} />
         </Link>
@@ -20,13 +41,11 @@ const SideBar = () => {
         <Link to="/">
           <FaRegTrashAlt color="white" size={20} />
         </Link>
-      </div>
-      <div className="self-center">
         <SignedIn>
           <UserButton />
         </SignedIn>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
